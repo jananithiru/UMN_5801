7394a4aa5e9a1184be391fe0f47ea0d0a09f2246
Bruce Gu <gtleclipse@gmail.com>
2015-11-29 12:05:12 -0600

    add admin method

---
+++ 5801_SCRS/src/scrs/Admin.java
@@ -0,0 +1,112 @@
+package scrs;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import scrs.Constants.PrimitiveDataType;
+import scrs.ShibbolethAuth.Token;
+
+public class Admin extends Person {
+
+	public boolean adminAddClass(ShibbolethAuth.Token token, int courseID, String courseName, int courseCredits,
+			String instructor, String firstDay, String lastDay, String classBeginTime, String classEndTime,
+			String weekDays, String location, String type, String prerequisite, String description, String department)
+					throws SQLException {
+		if (token.type != Token.RoleType.ADMIN) {
+			return false;
+		}
+
+		DBCoordinator dbcoordinator = new DBCoordinator();
+		String sqlCmd = null;
+		sqlCmd = "INSERT INTO COURSE TABLE VALUES ID " + courseID + courseName + courseCredits + firstDay + lastDay
+				+ classBeginTime + classEndTime + weekDays + location + type + prerequisite + description + department
+				+ ")";
+		ArrayList<String> dataList = null;
+		ArrayList<PrimitiveDataType> typeList = null;
+		dbcoordinator.insertData(sqlCmd, dataList, typeList);
+
+		return true;
+
+	}
+
+	public boolean adminDeleteClass(ShibbolethAuth.Token token, int courseID) throws SQLException {
+		if (token.type != Token.RoleType.ADMIN) {
+			return false;
+		}
+
+		DBCoordinator dbcoordinator = new DBCoordinator();
+		String sqlCmd = null;
+		sqlCmd = "DELETE DATA FROM COURSETABLE WHERE ID =" + courseID;
+		ArrayList<String> dataList = null;
+		ArrayList<PrimitiveDataType> typeList = null;
+		dbcoordinator.deleteData(sqlCmd, dataList, typeList);
+
+		return false;
+
+	}
+
+	public boolean adminEditClass(ShibbolethAuth.Token token, int courseID, String courseName, int courseCredits,
+			String instructor, String firstDay, String lastDay, String classBeginTime, String classEndTime,
+			String weekDays, String location, String type, String prerequisite, String description, String department)
+					throws SQLException {
+
+		if (token.type != Token.RoleType.ADMIN) {
+			return false;
+		}
+
+		DBCoordinator dbcoordinator = new DBCoordinator();
+
+		String sqlCmd = null;
+		sqlCmd = "UPDATE COURSETABLE SET" + "ID =" + courseID + "NAME =" + courseName + "CREDITS = " + courseCredits
+				+ "FIRSTDAY = " + firstDay + "LASTDAY = " + lastDay + "CLASSBEGINTIME = " + classBeginTime
+				+ "CLASSENDTIME =" + classEndTime + "ROUTINES =" + weekDays + "LOCATION +" + location + "TYPE = " + type
+				+ "PREREQUISITE =" + prerequisite + "DESCRIPTION =" + description + "DEPARTMENT = " + department;
+		ArrayList<String> dataList = null;
+		ArrayList<PrimitiveDataType> typeList = null;
+		dbcoordinator.updateData(sqlCmd, dataList, typeList);
+
+		return true;
+	}
+
+	public boolean adminAddStudentToClass(ShibbolethAuth.Token token, int studentID, int courseID, String grading,
+			String courseTerm) throws SQLException {
+
+		if (token.type != Token.RoleType.ADMIN) {
+			return false;
+		}
+
+		DBCoordinator dbcoordinator = new DBCoordinator();
+
+		String sqlCmd = null;
+
+		sqlCmd = "INSERT INTO STUDENTANDCOURSE VALUES (" + studentID + courseID + grading + courseTerm + ")";
+		ArrayList<String> dataList = null;
+		ArrayList<PrimitiveDataType> typeList = null;
+
+		dbcoordinator.insertData(sqlCmd, dataList, typeList);
+
+		return true;
+
+	}
+
+	public boolean adminEditStudentRegisteredClass(ShibbolethAuth.Token token, int studentID, int courseID,
+			String grading, String courseTerm) throws SQLException {
+		if (token.type != Token.RoleType.ADMIN) {
+			return false;
+		}
+
+		DBCoordinator dbcoordinator = new DBCoordinator();
+
+		String sqlCmd = null;
+		sqlCmd = "UPDATE STUDENTANDCOURSE SET ID =" + studentID + "SET COURSEID =" + courseID + "SET GRADING = "
+				+ grading + "SET COURSETERM =" + courseTerm;
+		ArrayList<String> dataList = null;
+		ArrayList<PrimitiveDataType> typeList = null;
+
+		dbcoordinator.updateData(sqlCmd, dataList, typeList);
+
+		return true;
+
+	}
+
+}
--- 5801_SCRS/src/scrs/SCRSImpl.java
+++ 5801_SCRS/src/scrs/SCRSImpl.java
@@ -1,5 +1,6 @@
 package scrs;
 
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,47 +9,48 @@ import java.util.Date;
 import java.util.Iterator;
 
 public class SCRSImpl implements mySCRS {
-	
+
 	public List<ArrayList<String>> queryStudentPersonalData(Token token, int studentID) {
-		//TODO: //Assuming the request is already verified as student. 
-		// Done. IN higher class if this session belongs to a student then query student personal data 
-		// iff] student is present in the student tb]\\avle, 
-		
-		// TODO: Create new DBCoodinator object here or elsewhere ? 
+		// TODO: //Assuming the request is already verified as student.
+		// Done. IN higher class if this session belongs to a student then query
+		// student personal data
+		// iff] student is present in the student tb]\\avle,
+
+		// TODO: Create new DBCoodinator object here or elsewhere ?
 		DBCoordinator dbcoordinator = new DBCoordinator();
-		
-		// TODO: Come up with this string escaped corrected 
+
+		// TODO: Come up with this string escaped corrected
 		String sqlStr = "select * from student where id=" + studentID;
-		
+
 		List<ArrayList<Object>> objList = dbcoordinator.queryData(sqlStr);
-		
-		List<ArrayList <String>> result = new ArrayList<ArrayList<String>>();
-		
-		//TODO: Type case from Object to String
+
+		List<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+
+		// TODO: Type case from Object to String
 		Iterator<ArrayList<Object>> outerIter = objList.iterator();
-	        while(outerIter.hasNext()){
-	        	ArrayList <String> metaRes = new ArrayList<String>(); 
-	        	Iterator<Object> innerIter = outerIter.next().iterator();
-	        	while(innerIter.hasNext()){
-	        		metaRes.add(innerIter.next().toString());
-	        		//System.out.println(innerIter.next());
-	        	}
-	        	result.add(metaRes);
-	        }
-	        
+		while (outerIter.hasNext()) {
+			ArrayList<String> metaRes = new ArrayList<String>();
+			Iterator<Object> innerIter = outerIter.next().iterator();
+			while (innerIter.hasNext()) {
+				metaRes.add(innerIter.next().toString());
+				// System.out.println(innerIter.next());
+			}
+			result.add(metaRes);
+		}
+
 		return result;
 	}
 
 	@Override
 	public Token userLogin(String x500, String password) {
-		
-		ShibbolethAuth sbAuth = new ShibbolethAuth(); 
-		
+
+		ShibbolethAuth sbAuth = new ShibbolethAuth();
+
 		if (x500 == null || password == null)
-			return null; //TODO: Check if we need to throw exceptions 
-		
+			return null; // TODO: Check if we need to throw exceptions
+
 		Token myToken = sbAuth.tokenGenerator(x500, password);
-		
+
 		return myToken;
 	}
 
@@ -57,4 +59,75 @@ public class SCRSImpl implements mySCRS {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	
+	// admin method
+
+	public boolean adminAddClass(Token token, int courseID, String courseName, int courseCredits, String instructor,
+			String firstDay, String lastDay, String classBeginTime, String classEndTime, String weekDays,
+			String location, String type, String prerequisite, String description, String department) {
+		// TODO Auto-generated method stub
+		Admin admin = new Admin();
+		try {
+			admin.adminAddClass(token, courseID, courseName, courseCredits, instructor, firstDay, lastDay,
+					classBeginTime, classEndTime, weekDays, location, type, prerequisite, description, department);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return true;
+	}
+
+	public boolean adminDeleteClass(Token token, int courseID) {
+		// TODO Auto-generated method stub
+		Admin admin = new Admin();
+		try {
+			admin.adminDeleteClass(token, courseID);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return true;
+	}
+
+	public boolean adminEditClass(Token token, int courseID, String courseName, int courseCredits, String instructor,
+			String firstDay, String lastDay, String classBeginTime, String classEndTime, String weekDays,
+			String location, String type, String prerequisite, String description, String department) {
+		// TODO Auto-generated method stub
+		Admin admin = new Admin();
+		try {
+			admin.adminEditClass(token, courseID, courseName, courseCredits, instructor, firstDay, lastDay,
+					classBeginTime, classEndTime, weekDays, location, type, prerequisite, description, department);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return true;
+	}
+
+	public boolean adminAddStudentToClass(Token token, int studentID, int courseID, String grading, String courseTerm) {
+		// TODO Auto-generated method stub
+		Admin admin = new Admin();
+		try {
+			admin.adminAddStudentToClass(token, studentID, courseID, grading, courseTerm);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return true;
+	}
+
+	public boolean adminEditStudentRegisteredClass(Token token, int studentID, int courseID, String grading,
+			String courseTerm) {
+		// TODO Auto-generated method stub
+		Admin admin = new Admin();
+		try {
+			admin.adminEditStudentRegisteredClass(token, studentID, courseID, grading, courseTerm);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return true;
+	}
+
 }
